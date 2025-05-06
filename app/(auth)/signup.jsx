ca17236dd534db5dad15f9276ca98cd070e2b6cc
fadoshi <doshiforama@gmail.com>
2025-05-06 17:31:46 -0500

    Root layout setup

---
+++ app/(auth)/signup.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Signup() {
+  return (
+    <View>
+      <Text> Signup Screen for the app</Text>
+    </View>
+  )
+}
---
+++ app/index.jsx
@@ -0,0 +1,30 @@
+import { StyleSheet, View } from "react-native";
+import { Link } from "expo-router";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      
+
+      <Link href="/(auth)/signup" style={styles.link}>Signup</Link>
+      <Link href="/(auth)" style={styles.link}>Login</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 18,
+    marginBottom: 20,
+  },
+  link: {
+    fontSize: 16,
+    color: "blue",
+    marginTop: 10,
+  },
+});
